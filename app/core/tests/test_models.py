29341a9ccd3d9997ddf9f210f45a65c2a0926e43
Jeff Marks <jeff.t.marks@gmail.com>
2024-01-22 13:30:04 -0500

    Complete tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,6 +13,12 @@ USER_DETAILS = {
     'password':'testpass123'
 }
 
+
+def create_user(email=USER_DETAILS['email'], password=USER_DETAILS['password']):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -60,3 +66,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
